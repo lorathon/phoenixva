7a83e59de2ae33fec3fb0f8cf518305614ed7015
Chuck Topinka <ctopinka@cfl.rr.com>
2014-09-20 10:20:40 -0400

    Optimizing autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session');
+$autoload['libraries'] = array();
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html', 'my', 'breadcrumb');
+$autoload['helper'] = array();
 
 
 /*
@@ -80,7 +80,7 @@ $autoload['helper'] = array('url', 'html', 'my', 'breadcrumb');
 |
 */
 
-$autoload['config'] = array('pva_config');
+$autoload['config'] = array();
 
 
 /*
--- application/core/PVA_Controller.php
+++ application/core/PVA_Controller.php
@@ -2,186 +2,189 @@
 
 /**
  * PVA Application base controller.
- * 
+ *
  * Ensures the database is current with the application and automatically
  * handles login redirection and admin checks. Private areas of the application
  * should have a URL like domain.com/private/ and admin areas should have a URL
  * like domain.com/admin/. All other URLs will be treated as public.
- * 
+ *
  * General controller usage should be to instantiate and populate models, set the
  * necessary values in the $data array, and then call $this->_render('view_name');
- * where 'view_name' = the view to render. This will automatically render the 
+ * where 'view_name' = the view to render. This will automatically render the
  * view within the appropriate template.
- * 
+ *
  * @author Chuck Topinka
  *
- */
+*/
 class PVA_Controller extends CI_Controller {
-    
+
 	public $data     = array();
 	private $_access = 'public';
-    
-    function __construct()
-    {
-        parent::__construct();
-        
-        // Styling and profiling
-        $this->output->enable_profiler(TRUE);
-        
-        log_message('debug', 'PVA Controller class initialized');
-        
-        // Register autoloader
-        spl_autoload_register(array('PVA_Controller','_autoload'));
-                
-        // Ensure database is current
-        $this->load->library('migration');
-        
-        if ( ! $this->migration->current())
-        {
-        	show_error($this->migration->error_string());
-        	log_message('error', 'Migration error: '.$this->migration->error_string());
-        }
-        
-        // Load PVA config file
-        $this->config->load('pva_config');        
-        
-        $this->data['errors'] = array();
-        $this->data['site_name'] = config_item('site_name');
-        $this->data['meta_title'] = config_item('site_name');
-        
-        // Public, private or admin access
-        $access = $this->uri->segment(1);
-        
-        if ($access == 'admin' OR $access == 'private')
-        {
-        	// Verify user logged in
-        	$this->library->load('tank_auth');
-        	
-        	if ( ! $this->tank_auth->is_logged_in())
-        	{
-        		// User not logged in so redirect to login
-        		redirect('/auth/login/');
-        	}
-        	
-        	// Logged In User's userdata
-        	$this->data['userdata'] = $this->session->all_userdata();
-        }
-        
-        if ($access == 'admin')
-        {
-        	if($this->data['userdata']['admin'] < 2)
-        	{
-        		// NO admin Credentials found.  Redirect to UNAUTH page
-        		redirect('/auth/unauth/');
-        	}
-        	
-        	// Load Admin config file
-        	$this->config->load('admin/admin_config');
-        	
-        	// Indicate admin area
-        	$this->data['site_name']  .= ' - Admin';
-        	$this->data['meta_title'] .= ' - Admin';
-        	$this->_access = 'admin';
-        }
-        elseif ($access == 'private')
-        {
-        	// Any common stuff for private access goes here.
-        	$this->_access = 'private';
-        }
-        
-        
-        
-        /* FRONT END ALERT
-         * If a session message is set shove into main data array
-         * Change to read ('item', 'value') or an array?
-         * OPTIONS:
-         * $this->session->set_flashdata('success', 'MESSAGE'); Green
-         * $this->session->set_flashdata('warning', 'MESSAGE'); Amber
-         * $this->session->set_flashdata('error', 'MESSAGE'); Red
-         * $this->session->set_flashdata('info', 'MESSAGE'); Blue
-        */
-        
-        /* ADMIN Alert */
-        if($this->session->flashdata('alert_message') != '')
-        {
-            $this->data['alert'] = TRUE;
-            $this->data['alert_type'] = $this->session->flashdata('alert_type');
-            $this->data['alert_message'] = $this->session->flashdata('alert_message');
-        }
-        else
-        {
-            $this->data['alert'] = FALSE;
-        }
-    }
-    
-    /**
-     * Renders views as part of overall template
-     * 
-     * This method should be called to display a view to ensure it is wrapped in
-     * the template as appropriate. If an array of views is loaded, the views
-     * will be rendered in the order set in the array.
-     * 
-     * @param string|array $view The view(s) to render. Optional. If not provided,
-     * a view with the same name as the current controller will be rendered.
-     */
-    protected function _render($view = NULL)
-    {
-    	// Default view to current class
-    	if (is_null($view)) $view = get_class($this);
-    	
-    	// Get the view(s) and place in view_output
-    	if (is_array($view))
-    	{
-    		foreach ($view as $subview)
-    		{
-    			log_message('debug', 'Rendering '.$subview);
-    			$this->data['view_output'] .= $this->load->view($view, $this->data, TRUE);
-    		}
-    	}
-    	else 
-    	{
-    		log_message('debug', 'Rendering '.$view);
-    		$this->data['view_output'] = $this->load->view($view, $this->data, TRUE);
-    	}
-    	
-    	// Render the appropriate template
-    	if ($this->_access == 'admin')
-    	{
-    		// Load the admin template
-    		log_message('debug', 'Using admin template.');
-    		$this->load->view('templates/admin', $this->data);
-    	}
-    	else 
-    	{
-    		// Load the site template
-    		log_message('debug', 'Using site template.');
-    		$this->load->view('templates/pva', $this->data);
-    	}
-    }
 
-    /**
-     * PVA Application autoloader
-     * 
-     * Allows calling objects without having to load models first.
-     * XXX Not sure this is the best way to do this.
-     * @param string $class The class name to load.
-     */
-    protected function _autoload($class)
-    {
-   		if(strpos($class, 'CI') !== 0)
-   		{
-   			$path = array('core', 'libraries', 'models');
-   			foreach ($path as $dir)
-   			{
-   				$file = APPPATH . 'libraries/' . $class . '.php';
-   				if(file_exists($file) && is_file($file))
-   				{
-   					log_message('debug', 'Autoloading '.$file);
-   					@include_once($file);
-   				}
-   			}
-   		}    	 
-    }
+	function __construct()
+	{
+		parent::__construct();
+
+		// Styling and profiling
+		$this->output->enable_profiler(TRUE);
+
+		log_message('debug', 'PVA Controller class initialized');
+
+		// Register autoloader
+		spl_autoload_register(array('PVA_Controller','_autoload'));
+
+		// Ensure database is current
+		$this->load->library('migration');
+
+		if ( ! $this->migration->current())
+		{
+			show_error($this->migration->error_string());
+			log_message('error', 'Migration error: '.$this->migration->error_string());
+		}
+
+		// Load PVA config file
+		$this->load->config('pva_config');
+
+		$this->data['errors'] = array();
+		$this->data['site_name'] = config_item('site_name');
+		$this->data['meta_title'] = config_item('site_name');
+
+		// Public, private or admin access
+		$access = $this->uri->segment(1);
+
+		if ($access == 'admin' OR $access == 'private')
+		{
+			// Verify user logged in
+			$this->load->library('tank_auth');
+
+			if ( ! $this->tank_auth->is_logged_in())
+			{
+				// User not logged in so redirect to login
+				redirect('/auth/login/');
+			}
+
+			// Logged In User's userdata
+			$this->load->library('session');
+			$this->data['userdata'] = $this->session->all_userdata();
+
+			/* XXX Is this even used?
+			 * FRONT END ALERT
+			 * If a session message is set shove into main data array
+			 * Change to read ('item', 'value') or an array?
+			 * OPTIONS:
+			 * $this->session->set_flashdata('success', 'MESSAGE'); Green
+			 * $this->session->set_flashdata('warning', 'MESSAGE'); Amber
+			 * $this->session->set_flashdata('error', 'MESSAGE'); Red
+			 * $this->session->set_flashdata('info', 'MESSAGE'); Blue
+			 */
+
+			// ADMIN Alert
+			if($this->session->flashdata('alert_message') != '')
+			{
+				$this->data['alert'] = TRUE;
+				$this->data['alert_type'] = $this->session->flashdata('alert_type');
+				$this->data['alert_message'] = $this->session->flashdata('alert_message');
+			}
+			else
+			{
+				$this->data['alert'] = FALSE;
+			}
+		}
+
+		if ($access == 'admin')
+		{
+			if($this->data['userdata']['admin'] < 2)
+			{
+				// NO admin Credentials found.  Redirect to UNAUTH page
+				redirect('/auth/unauth/');
+			}
+
+			// Load Admin config file
+			$this->load->config('admin/admin_config');
+
+			// Indicate admin area
+			$this->data['site_name']  .= ' - Admin';
+			$this->data['meta_title'] .= ' - Admin';
+			$this->_access = 'admin';
+		}
+		elseif ($access == 'private')
+		{
+			// Any common stuff for private access goes here.
+			$this->_access = 'private';
+		}
+	}
+
+	/**
+	 * Renders HTML views as part of overall template
+	 *
+	 * This method should be called to display a view to ensure it is wrapped in
+	 * the template as appropriate. If an array of views is loaded, the views
+	 * will be rendered in the order set in the array.
+	 *
+	 * @param string|array $view The view(s) to render. Optional. If not provided,
+	 * a view with the same name as the current controller will be rendered.
+	 */
+	protected function _render($view = NULL)
+	{
+		// Default view to current class
+		if (is_null($view)) $view = get_class($this);
+		
+		// Load helpers
+		$this->load->helper('url');
+		 
+		// Get the view(s) and place in view_output
+		if (is_array($view))
+		{
+			foreach ($view as $subview)
+			{
+				log_message('debug', 'Rendering '.$subview);
+				$this->data['view_output'] .= $this->load->view($view, $this->data, TRUE);
+			}
+		}
+		else
+		{
+			log_message('debug', 'Rendering '.$view);
+			$this->data['view_output'] = $this->load->view($view, $this->data, TRUE);
+		}
+		 
+		// Render the appropriate template
+		if ($this->_access == 'admin')
+		{
+			// Load the admin template
+			log_message('debug', 'Using admin template.');
+			$this->load->view('templates/admin', $this->data);
+		}
+		else
+		{
+			// Load the site template
+			log_message('debug', 'Using site template.');
+			$this->load->view('templates/pva', $this->data);
+		}
+	}
+
+	/**
+	 * PVA Application autoloader
+	 *
+	 * Allows calling objects without having to load models first.
+	 * XXX Not sure this is the best way to do this.
+	 * @param string $class The class name to load.
+	 */
+	protected function _autoload($class)
+	{
+		if(strpos($class, 'CI') !== 0)
+		{
+			$path = array('core', 'libraries', 'models');
+			foreach ($path as $dir)
+			{
+				$file = APPPATH . 'libraries/' . $class . '.php';
+				if(file_exists($file) && is_file($file))
+				{
+					log_message('debug', 'Autoloading '.$file);
+					@include_once($file);
+				}
+			}
+		}
+	}
 }
 
 /* End of file PVA_Controller.php */
