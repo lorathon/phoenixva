227e8d9873c8dbb40e5e6836647b25efd234a105
Chuck Topinka <ctopinka@cfl.rr.com>
2014-11-29 12:17:11 -0500

    IPB User ID
    Was hardcoded and now it isn't.

--- application/controllers/private/profile.php
+++ application/controllers/private/profile.php
@@ -50,7 +50,7 @@ class Profile extends PVA_Controller {
 			$this->data['name'] = pva_id($user).' '.$user->name;
 			$this->data['birthday'] = $user->birthday;
 			$this->data['joined'] = strip_time($user->created);
-			$this->data['ipbuser_id'] = 0;
+			$this->data['ipbuser_id'] = $user->ipbuser_id;
 
 			// Premium user
 			$this->data['is_premium'] = $user->is_premium();
