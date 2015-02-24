c32b072e5ef9a9ace7b3d5e6b2a34ed8e8027684
gofly02 <dustin@abelltechvt.com>
2015-02-24 14:39:47 -0500

    Custom CSS changes, add prefetch.js to github
    Removed boxed body styling in CSS and added prefetch.js which is used to configure typeahead for schedule search (not working at the moment).

---
+++ assets/js/prefetch.js
@@ -0,0 +1,41 @@
+﻿var airports = new Bloodhound({
+		  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('typeAhead'),
+		  queryTokenizer: Bloodhound.tokenizers.whitespace,
+		  limit: 10,
+		  prefetch: 'assets/data/airports.json'
+		});
+		 
+		 
+		airports.initialize();
+
+		 
+		$('#airports .typeahead').typeahead({
+		  highlight: true
+		},
+		{
+		  name: 'airports',
+		  displayKey: 'typeAhead',
+		  valueKey: 'fs',
+		  source: airports.ttAdapter(),
+		});
+		
+		var airlines = new Bloodhound({
+		  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('typeAhead'),
+		  queryTokenizer: Bloodhound.tokenizers.whitespace,
+		  limit: 10,
+		  prefetch: 'assets/data/airlines.json'
+		});
+		 
+		 
+		airlines.initialize();
+
+		 
+		$('#airlines .typeahead').typeahead({
+		  highlight: true
+		},
+		{
+		  name: 'airlines',
+		  displayKey: 'typeAhead',
+		  valueKey: 'fs',
+		  source: airlines.ttAdapter(),
+		});
