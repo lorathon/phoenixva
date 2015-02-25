fdb3936569a482f951ba3e65b4ddeec78f3a8f54
Chuck Topinka <ctopinka@cfl.rr.com>
2015-02-25 18:41:26 -0500

    Autoloader changes
    Changed the autoloader to account for CI system files. Fixes #76.

--- application/core/PVA_Controller.php
+++ application/core/PVA_Controller.php
@@ -231,24 +231,15 @@ class PVA_Controller extends CI_Controller {
 	 * PVA Application autoloader
 	 *
 	 * Allows calling objects without having to load models first.
-	 * XXX Not sure this is the best way to do this.
 	 * @param string $class The class name to load.
 	 */
 	protected function _autoload($class)
 	{
 		if (substr($class,0,4) == 'PVA_')
 		{
-			log_message('debug', 'Autoloading core class '.$class);
+			log_message('debug', 'Autoloading PVA core class '.$class);
 			$file = APPPATH.'core/'.$class.'.php';
-			if (file_exists($file) && is_file($file))
-			{
-				log_message('debug', 'Autoloading file '.$file);
-				@include_once($file);
-			}
-			else 
-			{
-				log_message('debug', 'Failed to load file '.$file);
-			}
+			$this->load_file($file);
 		}
 		elseif (substr($class,0,3) != 'CI_')
 		{
@@ -258,18 +249,29 @@ class PVA_Controller extends CI_Controller {
 			{
 				$file = APPPATH.$dir.'/'.strtolower($class).'.php';
 				log_message('debug', 'Looking for file '.$file);
-				if(file_exists($file) && is_file($file))
+				if($this->load_file($file))
 				{
-					log_message('debug', 'Autoloading file '.$file);
-					@include_once($file);
 					break;
 				}
 			}
 		}
 		else 
 		{
-			log_message('debug', 'Autoload called for CI core file--ignoring');
+			log_message('debug', 'Autoloading CI core class '.$class);
+			$file = BASEPATH.'core/'.substr($class,3).'.php';
+			$this->load_file($file);
 		}
+	}
+	
+	private function load_file($file)
+	{
+		if (file_exists($file) && is_file($file))
+		{
+			log_message('debug', 'Autoloading file '.$file);
+			@include_once($file);
+			return TRUE;
+		}
+		return FALSE;
 	}
 	
 	/**
