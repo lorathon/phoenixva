b90353d314a7e97411b084347d9c195b29cc5ddf
Chuck Topinka <ctopinka@cfl.rr.com>
2015-03-28 08:54:45 -0400

    Text email tweaks

--- application/views/email/transfer_approval-txt.php
+++ application/views/email/transfer_approval-txt.php
@@ -1,11 +1,12 @@
 Hello <?php echo $user->name; ?>,
 
 Welcome to the <?php echo $hub->get_full_name(); ?> crew center!
-Your request to transfer to <?php echo $hub->get_full_name(); ?> has been
-approved by <?php echo $userdata['rank_short'] . ' ' . $userdata['name'] ?>.
 
-Please contact your new <?php echo $hub->get_full_name(); ?> crew center 
-manager	if you have any questions.
+Your request to transfer to <?php echo $hub->get_full_name(); ?> has
+been approved by <?php echo $userdata['rank_short'] . ' ' . $userdata['name'] ?>.
+
+Please contact your new <?php echo $hub->get_full_name(); ?> crew
+center manager if you have any questions.
 
 Thank you!
 Phoenix Virtual Airways
--- application/views/email/transfer_pilot-txt.php
+++ application/views/email/transfer_pilot-txt.php
@@ -1,17 +1,20 @@
 Hello <?php echo $user->name; ?>,
 
-We received a request to transfer you to <?php echo $hub->get_full_name(); ?>.
-If you made this request there is nothing more you need to do. The staff will
-review your request and process it accordingly.
+We received a request to transfer you to: 
+<?php echo $hub->get_full_name(); ?>
+If you made this request there is nothing more you need to do. The 
+staff will review your request and process it accordingly.
 
-You can continue to fly while your transfer request is pending. You will be 
-notified when your request is accepted or denied.
+You can continue to fly while your transfer request is pending. You 
+will be notified when your request is accepted or denied.
 
 Please Note:
-You can only have one active transfer request at a time. You will not be able to
-request another transfer until your current request is accepted or denied.
+You can only have one active transfer request at a time. You will 
+not be able to request another transfer until your current request
+is accepted or denied.
 
-Please contact your current crew center manager if you have any questions.
+Please contact your current crew center manager if you have any
+questions.
 
 Thank you!
 Phoenix Virtual Airways
