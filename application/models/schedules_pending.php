baf6679284b6c92263a0d7457099cca60d39d98b
gofly02 <dustin@abelltechvt.com>
2015-04-06 22:15:44 -0400

    New Schedules API script

--- application/models/schedules_pending.php
+++ application/models/schedules_pending.php
@@ -9,30 +9,19 @@ class Schedules_pending extends PVA_Model {
 	public $dep_airport     = NULL;
 	public $arr_airport     = NULL;
 	public $equip           = NULL;
-	public $tail_number 	= NULL;
-	public $flight_type     = NULL;
+	public $service_type    = NULL;
 	public $regional        = NULL;
+        public $brand           = NULL;
 	public $dep_time_local  = NULL;
 	public $dep_time_utc    = NULL;
 	public $dep_terminal    = NULL;
-	public $dep_gate        = NULL;
-	public $block_time      = NULL;
-	public $taxi_out_time   = NULL;
-	public $air_time        = NULL;
-	public $taxi_in_time    = NULL;	
+	public $block_time      = NULL;	
 	public $arr_time_local  = NULL;
 	public $arr_time_utc    = NULL;
 	public $arr_terminal    = NULL;
-	public $arr_gate        = NULL;
-	public $downline_apt	= NULL;
-        public $sun             = NULL;
-        public $mon             = NULL;
-        public $tue             = NULL;
-        public $wed             = NULL;
-        public $thu             = NULL;
-        public $fri             = NULL;
-        public $sat             = NULL;
         public $version         = NULL;
+        public $created         = NULL;
+        
 
 	
 	
