3558715466e7513878d0bfe40f05dbe60598984e
Jeffrey Kobus <lorathon@prodigy.net>
2015-05-06 08:52:52 -0700

    Change to GET for event calendar

---
+++ application/views/js/events.php
@@ -0,0 +1,56 @@
+<script type="text/javascript">
+
+(function ($) {
+
+    'use strict';
+
+    var initCalendar = function () {
+        var $calendar = $('#calendar');
+
+        $calendar.fullCalendar({
+            header: {
+                left: 'title',
+                right: 'prev,today,next,basicDay,basicWeek,month'
+            },
+            timeFormat: '', // 'HH:mm / ' for 24 hour time (only shows start time on event)
+
+            titleFormat: {
+                month: 'MMMM YYYY', // September 2009
+                week: 'MMMM D, YYYY', // September 1-7, 2009
+                day: 'dddd, MMMM D, YYYY' 	// Tuesday, September 8, 2009
+            },
+            themeButtonIcons: {
+                prev: 'fa fa-caret-left',
+                next: 'fa fa-caret-right',
+            },
+            eventSources: [
+                {
+                    url: '<?php echo base_url();?>events/get-json',
+                    type: 'GET'
+                }
+            ]
+        });
+
+        // FIX INPUTS TO BOOTSTRAP VERSIONS
+        var $calendarButtons = $calendar.find('.fc-header-right > span');
+        $calendarButtons
+                .filter('.fc-button-prev, .fc-button-today, .fc-button-next')
+                .wrapAll('<div class="btn-group mt-sm mr-md mb-sm ml-sm"></div>')
+                .parent()
+                .after('<br class="hidden"/>');
+
+        $calendarButtons
+                .not('.fc-button-prev, .fc-button-today, .fc-button-next')
+                .wrapAll('<div class="btn-group mb-sm mt-sm"></div>');
+
+        $calendarButtons
+                .attr({'class': 'btn btn-sm btn-default'});
+    };
+
+    $(function () {
+        initCalendar();
+    });
+
+}).apply(this, [jQuery]);
+
+</script>
