84667b6f4d90224a8d0b238d3b8517418bd79428
Jeffrey Kobus <lorathon@prodigy.net>
2015-09-10 13:52:11 -0700

    Addition of about page.
    Wording needs some fine tuning but it is setup.

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Static page controller
@@ -9,84 +11,97 @@
  * @author Chuck
  *
  */
-class Pages extends PVA_Controller {
-	
-	/**
-	 * Finds the page to render.
-	 * 
-	 * @param string $page The page to render.
-	 */
-	public function view($page = 'home')
+class Pages extends PVA_Controller
+{
+
+    /**
+     * Finds the page to render.
+     * 
+     * @param string $page The page to render.
+     */
+    public function view($page = 'home')
+    {
+	$this->load->helper('url');
+
+	log_message('debug', 'Pages Controller viewing ' . $page);
+
+	// Load the session
+	$this->load->library('session');
+
+	// Handle special pages
+	if (substr($page, 0, 4) == 'hub-')
 	{
-                $this->load->helper('url');
-                
-                log_message('debug','Pages Controller viewing '.$page);
-		
-		// Load the session
-		$this->load->library('session');
-		
-		// Handle special pages
-		if (substr($page, 0, 4) == 'hub-')
-		{
-			$this->session->keep_flashdata('alerts');
-			redirect("hubs/{$page}");
-		}
-		
-		if (substr($page, 0, 6) == 'event-')
-		{
-			$this->session->keep_flashdata('alerts');
-			redirect("events/{$page}");
-		}
-		
-		// File or database
-		if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
-		{
-			// Try to get page from database
-			log_message('debug','Getting page from database: '.$page);
-			
-			$article = new Article();
-			$article->slug = $page;
-			$article->find();
-			
-			if ($article->title)
-			{
-				$this->data['title'] = $article->title;
-				$this->data['body'] = $article->body_html;
-				$this->data['pubdate'] = $article->pubdate;
-				$this->_render('article');
-				return FALSE;
-			}
-			else 
-			{
-				// No page in database, no page exists
-				show_404();
-			}
-		}
-		
-                // default page title
-                $this->data['title'] = ucwords($page);
-                
-		
-                if ($page == 'home')
-		{                        
-                        // page specific stylesheets
-                        $this->data['stylesheets'][] = base_url('assets/vendor/owlcarousel/owl.carousel.css');
-                        $this->data['stylesheets'][] = base_url('assets/vendor/owlcarousel/owl.theme.css');
-                        $this->data['stylesheets'][] = base_url('assets/vendor/rs-plugin/css/settings.css');
-                        $this->data['stylesheets'][] = base_url('assets/vendor/circle-flip-slideshow/css/component.css');                        
-                        
-                        // page specific javascripts
-			$this->data['scripts'][] = base_url('assets/vendor/owlcarousel/owl.carousel.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/owlcarousel/owl.carousel.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/rs-plugin/js/jquery.themepunch.tools.min.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/jflickrfeed/jflickrfeed.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/jquery.appear/jquery.appear.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/jquery.easing/jquery.easing.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/jquery-cookie/jquery-cookie.js');
-                        $this->data['scripts'][] = base_url('assets/vendor/jquery.validation/jquery.validation.js');
-		}
-                
-                $this->_render('pages/'.$page);
+	    $this->session->keep_flashdata('alerts');
+	    redirect("hubs/{$page}");
 	}
+
+	if (substr($page, 0, 6) == 'event-')
+	{
+	    $this->session->keep_flashdata('alerts');
+	    redirect("events/{$page}");
+	}
+
+	// File or database
+	if (!file_exists(APPPATH . '/views/pages/' . $page . '.php'))
+	{
+	    // Try to get page from database
+	    log_message('debug', 'Getting page from database: ' . $page);
+
+	    $article = new Article();
+	    $article->slug = $page;
+	    $article->find();
+
+	    if ($article->title)
+	    {
+		$this->data['title'] = $article->title;
+		$this->data['body'] = $article->body_html;
+		$this->data['pubdate'] = $article->pubdate;
+		$this->_render('article');
+		return FALSE;
+	    }
+	    else
+	    {
+		// No page in database, no page exists
+		show_404();
+	    }
+	}
+
+	// default page title
+	$this->data['title'] = ucwords($page);
+
+
+	if ($page == 'home')
+	{
+	    // page specific stylesheets
+	    $this->data['stylesheets'][] = base_url('assets/vendor/owlcarousel/owl.carousel.css');
+	    $this->data['stylesheets'][] = base_url('assets/vendor/owlcarousel/owl.theme.css');
+	    $this->data['stylesheets'][] = base_url('assets/vendor/rs-plugin/css/settings.css');
+	    $this->data['stylesheets'][] = base_url('assets/vendor/circle-flip-slideshow/css/component.css');
+
+	    // page specific javascripts
+	    $this->data['scripts'][] = base_url('assets/vendor/owlcarousel/owl.carousel.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/owlcarousel/owl.carousel.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/rs-plugin/js/jquery.themepunch.tools.min.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jflickrfeed/jflickrfeed.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery.appear/jquery.appear.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery.easing/jquery.easing.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery-cookie/jquery-cookie.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery.validation/jquery.validation.js');
+	}
+
+	if ($page == 'about')
+	{
+	    // page specific stylesheets
+
+	    // page specific javascripts
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery.appear/jquery.appear.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/jquery.easing/jquery.easing.js');
+	    $this->data['scripts'][] = base_url('assets/vendor/modernizr/modernizr.js');
+	    $this->data['scripts'][] = base_url('assets/js/theme.init.js');
+	}	
+
+	$this->_render('pages/' . $page);
+    }
+
 }
---
+++ application/views/pages/about.php
@@ -0,0 +1,133 @@
+<div class="container">
+    <div class="row center">
+	<div class="col-md-12">
+	    <h2 class="short word-rotator-title">At PVA you can fly a <strong class="inverted" data-appear-animation="bounceIn">
+		    <span class="word-rotate" data-plugin-options='{"delay": 2000}'>
+			<span class="word-rotate-items">
+			    <span>Jumbo Jet.</span>
+			    <span>Helicopter.</span>
+			    <span>Sea Plane.</span>
+			    <span>Regional Jet.</span>
+			    <span>Cessna 172</span>
+			</span>
+		    </span>
+		</strong>
+	    </h2>
+	    <p class="featured lead">
+		We have attempted to build a community that carters to all pilots.  If you have just started flying, this is the place you want to start.
+		There are many experienced Virtual Pilots here that will help you get up and running with the basics.  If you are experienced, you
+		can instruct others or maybe even hone your skills with fellow experienced pilots.  Those pilot in between will have all of their needs
+		fulfilled.  Our forum of pilots discuss all aspects of flying.  We have a tight knit group of pilots that will be there when you need them.
+		Our <span class="alternative-font">events</span> span from a tour of all the crew centers, to flying an outdated airframe around the world.
+	    </p>
+	</div>
+    </div>
+    <hr class="tall" />
+</div>
+
+<div class="container">
+    <div class="row">
+	<div class="col-md-12">
+	    <div class="row">
+		<div class="col-md-4">
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-group"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">Community</h4>
+			    <p class="tall">blah blah blah about the forum</p>
+			</div>
+		    </div>
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-file"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">Events</h4>
+			    <p class="tall">blah blah blah about events</p>
+			</div>
+		    </div>
+		</div>
+		<div class="col-md-4">
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-film"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">Training</h4>
+			    <p class="tall">blah blah blah about training</p>
+			</div>
+		    </div>
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-check"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">Staff</h4>
+			    <p class="tall">blah blah blah about the staff.</p>
+			</div>
+		    </div>
+		</div>
+		<div class="col-md-4">
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-bars"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">Competition</h4>
+			    <p class="tall">We over various types of friendly competitions between pilots.  From your standard 
+			    landing competition. To competitions that focus on flying for a specific airline in a months
+			    time.</p>
+			</div>
+		    </div>
+		    <div class="feature-box secundary">
+			<div class="feature-box-icon">
+			    <i class="fa fa-desktop"></i>
+			</div>
+			<div class="feature-box-info">
+			    <h4 class="shorter">?????</h4>
+			    <p class="tall">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. 
+				Nulla lacinia iaculis nulla.</p>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </div>
+
+</div>
+
+<section class="featured highlight footer">
+    <div class="container">
+	<div class="row center counters">
+	    <div class="col-md-3 col-sm-6">
+		<strong data-to="224">0</strong>
+		<label>Active Pilots</label>
+	    </div>
+	    <div class="col-md-3 col-sm-6">
+		<strong data-to="5">0</strong>
+		<label>Years in Business</label>
+	    </div>
+	    <div class="col-md-3 col-sm-6">
+		<strong data-to="142109" data-append="+">0</strong>
+		<label>Total Flights</label>
+	    </div>
+	    <div class="col-md-3 col-sm-6">
+		<strong data-to="314625"data-append=":00">0</strong>
+		<label>Hours Flown</label>
+	    </div>
+	</div>
+    </div>
+</section>
+
+
+
+
+
+<script src="<?php echo base_url('assets/vendor/jquery/jquery.js')?>"></script>
+<script src="<?php echo base_url('assets/vendor/jquery.appear/jquery.appear.js')?>"></script>
+<script src="<?php echo base_url('assets/vendor/common/common.js')?>"></script>
+<script src="<?php echo base_url('assets/js/theme.js')?>"></script>
+<script src="<?php echo base_url('assets/js/theme.init.js')?>"></script>
+
